244a7c15dbc4c20144881324e9056d2e3e2f44fd
Simeón Konstantinov Petrov <simekons@ucm.es>
2020-04-07 18:59:08 +0200

    Añadidas partículas de la gravedad. VFX y SFX.

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    // Aquí se declaran los archivos de sonido que tenemos como variables para poder usarlos en el código.
+    public static AudioClip Gravedad;
+
+    // El componente del inspector que va a manejar los sonidos.
+    public static AudioSource audioSrc;
+
+    void Start()
+    {
+        // Es importante que la carpeta en la que estén los sonidos se llame Resources, ya que sino no lo busca.
+        
+        Gravedad = Resources.Load<AudioClip>("SonidoGravedad");
+        audioSrc = GetComponent<AudioSource>();
+        audioSrc.volume = 0.1f;
+
+    }
+
+  
+    public static void PlaySound (string clip)
+    {
+        switch (clip)
+        {
+            case "Gravedad":
+                
+                audioSrc.PlayOneShot(Gravedad);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/VFXGravedad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXGravedad : MonoBehaviour
+{
+    public Animator anim;
+    SoundManager audioSrc;
+    SoundManager Gravedad;
+    
+    void Update()
+    {
+        if (GameManager.instance.GetGravedad())
+        {
+            
+            anim.SetBool("Gravedad", true);
+            anim.SetBool("Gravedad2", false);
+           
+        }
+        else
+        {
+            
+            anim.SetBool("Gravedad", false);
+            anim.SetBool("Gravedad2", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1) && GameManager.instance.GetCapsulasRest() > 0 && !GameManager.instance.GetEscalera())
+            SoundManager.PlaySound("Gravedad");
+    }
+}
