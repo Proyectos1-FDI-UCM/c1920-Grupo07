827df5d7a8e115f3f3b3e440a3e4b52614879f36
androd27 <androd27@ucm.es>
2020-04-21 20:02:29 +0200

    Añadido la particula de andar

--- Assets/Scripts/Movimientos/PlayerController.cs
+++ Assets/Scripts/Movimientos/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer jug;
-    public ParticleSystem part;
+    public ParticleSystem part1;
+    public ParticleSystem part2;
 
     public float speed = 8.0f;
     public float jumpForce = 12.0f;    
@@ -96,6 +97,7 @@ public class PlayerController : MonoBehaviour
 
     private void CheckInput()
     {
+        Particula1();
         movimientoInput = Input.GetAxisRaw("Horizontal");
         
 
@@ -127,16 +129,44 @@ public class PlayerController : MonoBehaviour
     private void Particula()
     {
         if (isGrounded)
-            part.Play();
+            part1.Play();
 
         if(GameManager.instance.GetGravedad()&& isGrounded)
         {
-            part.transform.position = new Vector3(part.transform.position.x, transform.position.y + 0.6f, part.transform.position.z);
+            part1.transform.position = new Vector3(part1.transform.position.x, transform.position.y + 0.6f, part1.transform.position.z);
         }
         if (!GameManager.instance.GetGravedad() && isGrounded)
         {
-            part.transform.position = new Vector3(part.transform.position.x, transform.position.y - 0.8f, part.transform.position.z);
+            part1.transform.position = new Vector3(part1.transform.position.x, transform.position.y - 0.8f, part1.transform.position.z);
         }
 
     }
+    private void Particula1()
+    {
+        if (isWalking)
+            part2.Play();
+
+        if (GameManager.instance.GetGravedad() && isWalking)
+        {
+            part2.transform.position = new Vector3(part2.transform.position.x, transform.position.y + 0.6f, part2.transform.position.z);
+        }
+        if (!GameManager.instance.GetGravedad() && isWalking)
+        {
+            part2.transform.position = new Vector3(part2.transform.position.x, transform.position.y - 0.8f, part2.transform.position.z);
+        }
+
+        else if (GameManager.instance.GetParedL())
+        {
+            part2.Stop();
+        }
+         if (GameManager.instance.GetParedR())
+        {
+            part2.Stop();
+        }
+         else if (!isGrounded)
+        {
+            part2.Stop();
+        }
+    }
+
 }
