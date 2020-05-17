5e485a02df0e0400abb75d21634f659980515634
albgom21 <62644841+albgom21@users.noreply.github.com>
2020-05-17 18:32:41 +0200

    Añadida la plataforma móvil

---
+++ Assets/Scripts/PlataformaMovil.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaMovil : MonoBehaviour
+{
+    [SerializeField] private float dist, velocidad;
+
+    private Rigidbody2D rb;
+    private Vector2 velActual;
+
+    private float pos;
+
+    private bool cambio;
+    private bool recuperaVel = false;
+    private bool velAct = false;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        pos = transform.position.y;
+        rb.velocity = new Vector2(velocidad, 0);
+    }
+
+    void Update()
+    {
+        if (GameManager.instance.Tiempo())
+        {
+            if (!velAct)
+            {
+                velActual = rb.velocity;
+                velAct = true;
+            }
+            rb.velocity = new Vector2(0, 0);
+            recuperaVel = true;
+        }
+        else if (!GameManager.instance.Tiempo())
+        {
+            velAct = false;
+            if (recuperaVel)
+            {
+                rb.velocity = velActual;
+                recuperaVel = false;
+            }
+        }
+
+        if (transform.position.y > pos + dist)  //Controlar que no se pase de la distancia
+        {
+            cambio = true;
+        }
+
+        else if (transform.position.y < pos - dist)
+        {
+            cambio = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (!GameManager.instance.Tiempo())
+        {
+            if (cambio)
+                rb.velocity = new Vector2(0,-velocidad * 3);
+            else
+                rb.velocity = new Vector2(0,velocidad * 3);
+        }
+    }
+}
