ad98075477d5b4fba44457a601eb8cd50b65042a
Simeón Konstantinov Petrov <simekons@ucm.es>
2020-05-18 16:59:35 +0200

    Modificado el volumen del salto y del nivel 3. Cambios en scripts.

--- Assets/Scripts/Movimientos/GirarPlataforma.cs
+++ Assets/Scripts/Movimientos/GirarPlataforma.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GirarPlataforma : MonoBehaviour
 {
+    [SerializeField] public float rotacion;
     void Update()
     {
         if (GameManager.instance.Tiempo())
             this.transform.Rotate(0, 0, 0);
-        else this.transform.Rotate(0, 0, 5);
+        else this.transform.Rotate(0, 0, rotacion);
     }
 }
