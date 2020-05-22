9762d003c455f4524ec3f0d41ae8926c46787af9
Simeón Konstantinov Petrov <simekons@ucm.es>
2020-05-23 00:56:33 +0200

    Parte del nivel 4 hecha, corrección de la animación del murciélago, organización del proyecto.

---
+++ Assets/Scripts/Movimientos/Plataformas/PlataformaMovilXX.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaMovilXX : MonoBehaviour
+{
+    [SerializeField] private float dist, velocidad;
+
+    private Rigidbody2D rb;
+    private Vector2 velActual;
+
+    private float pos;
+
+    private bool cambio;
+    private bool recuperaVel = false;
+    private bool velAct = false;
+
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        pos = transform.position.x;
+    }
+
+    void Update()
+    {
+        if (GameManager.instance.Tiempo())
+        {
+            if (!velAct)
+            {
+                velActual = rb.velocity;
+                velAct = true;
+            }
+            rb.velocity = new Vector2(0, 0);
+            recuperaVel = true;
+        }
+        else if (!GameManager.instance.Tiempo())
+        {
+            velAct = false;
+            if (recuperaVel)
+            {
+                rb.velocity = velActual;
+                recuperaVel = false;
+            }
+        }
+
+        if (transform.position.x < pos - dist) // Controlar que no se pase de la distancia
+        {
+            cambio = true;
+        }
+
+        else if (transform.position.x > pos + dist) 
+        {
+            cambio = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (!GameManager.instance.Tiempo())
+        {
+            if (cambio)
+                rb.velocity = new Vector2(-velocidad, 0);
+            else
+                rb.velocity = new Vector2(velocidad, 0);
+        }
+    }
+}
